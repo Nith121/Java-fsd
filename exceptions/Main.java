dd72c72d16c325892df97e6bedf5eefdf7a0a349
Nithish <nithishreddysolleti81@gmail.com>
2023-10-27 17:58:17 +0530

    Multithreading Exception Handling and OOPS Practice Projects added

---
+++ exceptions/Main.java
@@ -0,0 +1,17 @@
+package com.practice.exceptions;
+
+public class Main 
+{ 
+    public static void main(String args[]) 
+    { 
+        try
+        { 
+            throw new MyException("temp"); 
+        } 
+        catch (MyException ex) 
+        { 
+            System.out.println("Caught"); 
+            System.out.println(ex.getMessage()); 
+        } 
+    } 
+}
